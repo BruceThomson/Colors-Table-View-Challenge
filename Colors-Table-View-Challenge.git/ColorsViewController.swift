a2de221286cf5550f2072adb37ec8890e5fc8212
Bruce Thomson <bwt5z6@mail.missouri.edu>
2017-07-09 17:33:21 -0500

    Built table view and view controller

---
+++ Colors-Table-View-Challenge.git/ColorsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ColorsViewController.swift
+//  Colors-Table-View-Challenge.git
+//
+//  Created by Bruce Thomson on 7/9/17.
+//  Copyright © 2017 Bruce Thomson. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+    @IBOutlet weak var colorsTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
